b15c5bb45b46110c2de6681f2dd3c488458a3bc1
Ubuntu <ubuntu@ip-172-31-20-251.ap-southeast-1.compute.internal>
2015-05-20 03:06:43 +0000

    add all

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ -f /usr/local/bin/pip ] || [ -f /usr/bin/pip ];
+then
+    echo "pip exists"
+else
+    echo "Installing pip"
+    wget https://bootstrap.pypa.io/get-pip.py
+    sudo python get-pip.py
+fi
+sudo apt-get update
+sudo apt-get install pptpd
+sudo cp ./pptpd.conf /etc/pptpd.conf
+sudo cp ./chap-secrets /etc/ppp/chap-secrets
+sudo cp ./pptpd-options /etc/ppp/pptpd-options
+sudo cp ./sysctl.conf /etc/sysctl.conf
+sudo sysctl -p
+sudo /etc/init.d/pptpd restart
+sudo apt-get install iptables
+sudo iptables -t nat -A POSTROUTING -s 192.168.117.0/24 -o eth0 -j MASQUERADE
+sudo cp ./rc.local /etc/init.d/rc.local
+sudo cp ./ufw /etc/default/ufw
+sudo reboot
