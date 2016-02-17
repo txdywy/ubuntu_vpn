eb848420cffd22c0898147b3cc9d8c10d59f36b5
Yi Wei <genius.ron@gmail.com>
2016-02-17 13:08:51 +0800

    add pm2 to node

--- deploy_node.sh
+++ deploy_node.sh
@@ -4,6 +4,8 @@ sudo apt-get install -y npm
 sudo apt-get install -y nodejs
 npm install http-proxy --save
 npm install url --save
+sudo npm install pm2 -g 
+sudo ln -s /usr/bin/nodejs /usr/bin/node
 
 #sudo apt-get install vnstat
 #sudo vnstat -u -i eth0
