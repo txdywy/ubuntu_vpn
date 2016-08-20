f8beab928aa7fb384541fbc8020c2b649193a950
Ubuntu <ubuntu@ip-172-31-20-154.ap-south-1.compute.internal>
2016-08-20 16:11:26 +0000

    up ss

--- ss.sh
+++ ss.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [ -f /usr/local/bin/pip ] || [ -f /usr/bin/pip ];
+then
+    echo "pip exists"
+else
+    echo "Installing pip"
+    wget https://bootstrap.pypa.io/get-pip.py
+    sudo python get-pip.py
+fi
 sudo pip install shadowsocks
 python ss.py
 sudo ssserver -c ss.json -d start
