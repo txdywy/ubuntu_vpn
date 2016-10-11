1a0b37e03d4675e432566603d9dc83830d1766bc
Yi Wei <genius.ron@gmail.com>
2016-10-11 18:40:26 +0800

    ad pip to ppxy

--- ppxy.sh
+++ ppxy.sh
@@ -5,5 +5,6 @@ cd Python-3.6.0b2/
 ./configure 
 make
 sudo make install
+sudo python3.6 -m pip install pproxy
 read -p "Please specify your ss pass: " pass
 nohup pproxy -i ss://chacha20:$pass@:1287 &
