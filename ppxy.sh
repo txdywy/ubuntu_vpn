8ef7a0c9fcbfd6363bead6146282352d9dc1eee4
Yi Wei <weiyi@papayamobile.com>
2016-10-11 18:32:33 +0800

    add ppxy

---
+++ ppxy.sh
@@ -0,0 +1,9 @@
+wget https://www.python.org/ftp/python/3.6.0/Python-3.6.0b2.tgz
+sudo apt-get install libssl-dev openssl
+tar -xvzf Python-3.6.0b2.tgz 
+cd Python-3.6.0b2/
+./configure 
+make
+sudo make install
+read -p "Please specify your ss pass: " pass
+nohup pproxy -i ss://chacha20:$pass@:1287 &
